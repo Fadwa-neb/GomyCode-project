7b876119a1c102bcd45b2f885020254a60239ebe
Fadwa-neb <fadwa.nebti@gmail.com>
2025-03-26 22:37:50 +0000

    Add files via upload

---
+++ DOMP2.js
@@ -0,0 +1,30 @@
+
+// Wait for the DOM content to be fully loaded
+
+document.addEventListener('DOMContentLoaded', function () {
+  
+  
+  // Select the color box and the button elements
+  
+  const colorBox = document.getElementById('color-box');
+  
+  const changeColorBtn = document.getElementById('change-color-btn');
+
+
+  function getRandomColor() {
+    const letters = '0123456789ABCDEF';
+    let color = '#';
+    for (let i = 0; i < 6; i++) {
+        color += letters[Math.floor(Math.random() * 16)];
+    }
+    return color;
+}
+
+changeColorBtn.addEventListener('click', function () {
+  
+  const randomColor = getRandomColor(); 
+  
+  colorBox.style.backgroundColor = randomColor; 
+});
+
+});
